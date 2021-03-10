e6859cfe4a1a3d32d036b5519988eca4e68c8210
Abangpa1ace <dyek72731@gmail.com>
2021-03-11 02:02:20 +0900

    Add: Detail 상세 페이지 다중색상 존재시 이미지 리스트 추가

--- client/src/Containers/Detail/components/DetailInfo/DetailInfo.jsx
+++ client/src/Containers/Detail/components/DetailInfo/DetailInfo.jsx
@@ -5,10 +5,12 @@ import DetailInfoPurchase from './DetailInfoPurchase';
 import DetailInfoExplain from './DetailInfoExplain';
 import DetailInfoPickup from './DetailInfoPickup';
 import DetailInfoFoldable from './DetailInfoFoldable';
+import DetailInfoColors from './DetailInfoColors';
 
 const DetailInfo = ({ 
-  setModalMode, model, name, category, 
+  setModalMode, id, images, model, name, category, 
   colors, price, sizes, explanation, review }) => {
+
   return (
     <Detailinfo>
       <InfoTitle>
@@ -18,6 +20,7 @@ const DetailInfo = ({
         </p>
         <h1>{name}</h1>
       </InfoTitle>
+      {(colors && colors.otherColors) && <DetailInfoColors id={id} image={images[0]} colors={colors} />}
       <DetailInfoPurchase setModalMode={setModalMode} sizes={sizes} />
       <DetailInfoPickup setModalMode={setModalMode} />
       <DetailInfoExplain model={model} colors={colors} explanation={explanation} />
