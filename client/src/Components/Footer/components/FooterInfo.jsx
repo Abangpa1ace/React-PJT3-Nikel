fc99f47e05866e741e568b27d2081f5875d08481
Abangpa1ace <dyek72731@gmail.com>
2021-03-13 05:34:49 +0900

    Modify: 공용 링크 FooterData.js 참조

--- client/src/Components/Footer/components/FooterInfo.jsx
+++ client/src/Components/Footer/components/FooterInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-router-dom';
 import { flexBetweenStart } from '../../../Styles/theme';
+import { FOOTER_LINK_CLIENT } from '../FooterData';
 
 const FooterInfo = () => {
   return (
@@ -9,7 +10,7 @@ const FooterInfo = () => {
       <div className="left">
         <p>(유)나이키코리아 대표 KIM MYUNG HEE | 서울 강남구 테헤란로 152 강남파이낸스센터 31층</p>
         <p>통신판매업신고번호 2011-서울강남-03461 | 등록번호 220-88-09068 <Link to="/">사업자정보확인</Link></p>
-        <p>고객센터 전화문의 <Link to="/">080-022-0182</Link></p>
+        <p>고객센터 전화문의 <Link to={FOOTER_LINK_CLIENT[0].link}>080-022-0182</Link></p>
         <p>FAX 02-6744-5880 | E-mail service@nike.co.kr | 호스팅서비스사업자 브리즈</p>
       </div>
       <div className="right">
