3bb94064f58f4ff69bca056c77ca172f21009cf2
Abangpa1ace <dyek72731@gmail.com>
2021-03-15 19:01:33 +0900

    Modify: Server Side Pagination 변환, Filtering 연동 테스트중

--- server/routes/items.js
+++ server/routes/items.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const itemsData = require('../database/itemsData');
+const LIMIT = 20;
 
 // List Router
 router.get('/:primary/:secondary', function(req, res) {
@@ -14,15 +15,21 @@ router.get('/:primary/:secondary', function(req, res) {
 
 router.get('/:primary/:secondary/:tertiary', function(req, res) {
   let itemList = filterByParams(itemsData, req.params);
-  console.log(req.query)
-  if (req.query) {
-    itemList = filterByQuery(req.query, itemList)
+  
+  const { round } = req.query;
+  let filterQuery = req.query;
+  delete filterQuery.round;
+  console.log(round, filterQuery);
+  if (filterQuery) {
+    itemList = filterByQuery(itemList, filterQuery)
   }
+  itemList = sliceByRound(itemList, Number(round))
   res.json({
-    itemList
+    itemList,
   });
 });
 
+// Semantic URL Handler
 const filterByParams = (list, params) => {
   const { primary, secondary, tertiary } = params;
   let newList = list.filter((item) => {
@@ -35,24 +42,26 @@ const filterByParams = (list, params) => {
   return newList;
 }
 
+// Query String Handler
 const queryObject = {
   // size: (list, values) => { return list.filter(item => }
   color: (list, values) => { return list.filter(item => values.includes(item.colors.code)) },
   brand: (list, values) => { return list.filter(item => values.includes(item.brand)) },
 }
 
-// List Functions
-const filterByQuery = (query, list) => {
+const filterByQuery = (list, query) => {
   query = Object.entries(query).map(e => [e[0], e[1].split(",")]);
-  console.log(list.length);
+  let newList = [...list];
   for (let q of query) {
-    list = queryObject[q[0]](list, q[1]);
+    newList = queryObject[q[0]](newList, q[1]);
   }
-  console.log(list.length);
-  return list;
+  return newList;
 }
 
-// Detail Router
-
+// List Slicer by Round
+const sliceByRound = (list, round) => {
+  const offset = round * LIMIT;
+  return list.slice(offset, offset + LIMIT);
+}
 
 module.exports = router;
